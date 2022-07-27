2d4181acb382394ca9ee6058d5f6d6025eac692c
wngml317 <wngml317@naver.com>
2022-07-27 12:27:58 +0900

    add

--- Tab/app/src/main/java/com/wngml/tab/MainActivity.java
+++ Tab/app/src/main/java/com/wngml/tab/MainActivity.java
@@ -1,14 +1,65 @@
 package com.wngml.tab;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView navigationView;
+
+    Fragment firstFragment;
+    Fragment secondFragment;
+    Fragment thirdFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 액션바 숨기는 코드
+        getSupportActionBar().hide();
+
+        navigationView = findViewById(R.id.bottomNavigationView);
+
+        firstFragment = new FirstFragment();
+        secondFragment = new SecondFragment();
+        thirdFragment = new ThirdFragment();
+
+        navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                int itemId = item.getItemId();
+
+                Fragment fragment = null;
+
+                if (itemId == R.id.firstFragment) {
+                    fragment = firstFragment;
+                } else if (itemId == R.id.secondFragment) {
+                    fragment = secondFragment;
+                } else if (itemId == R.id.thirdFragment) {
+                    fragment = thirdFragment;
+                }
+
+                return loadFragment(fragment);
+            }
+        });
+
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment, fragment).commit();
+            return true;
+        }
+        return false;
     }
 }
